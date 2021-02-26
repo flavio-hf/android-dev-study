be1118f0e734f26f25e11792afad2ba477b9ea8e
Flavio H·F <flavio.hferreira@outlook.com>
2021-02-26 07:39:00 -0300

    found temp workaround to run things on the Web. need to clarify compilation process and the inconsistency in main taking or not arguments

--- baby-steps/1_hello-world.kt
+++ baby-steps/1_hello-world.kt
@@ -1,3 +1,9 @@
-fun main(args : Array<String>) {
-    println("'Sup, Android? \n I'm coming for you!") 
+// consider this: «fun main(args : Array<String>)», it's taught this way at SoloLearn and though either Intellij IDEA or VS Code find any problem, it doesn't run on terminal and on Kotlin playground. I couldn't run the program without it either on terminal, but it worked on Playground. The error presented says that main never takes arguments.
+fun main() {
+    println("'Sup, Android? \n I'm coming for you!")
+    // data types max values calls
+    println(Int.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+    println(Double.MAX_VALUE)
+    println(Float.MAX_VALUE)
 }
