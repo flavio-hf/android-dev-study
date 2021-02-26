e4e743d7d6774daf15d8c962dee613878507148e
Flavio H·F <flavio.hferreira@outlook.com>
2021-02-26 08:31:23 -0300

    data types & data types conversion through dot notation

---
+++ baby-steps/2_data-types+conversion.kt
@@ -0,0 +1,13 @@
+fun main(){
+    // data types max values calls (when you need to know how big a variable can be)
+    println(Int.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+    println(Double.MAX_VALUE)
+    println(Float.MAX_VALUE)
+
+    // data types can be easily converted with dot notation
+    var number = 1
+    println(number)
+    // convert to float
+    println(number.toFloat())
+}
